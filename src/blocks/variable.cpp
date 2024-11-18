3f527f3e241400d9ab717f6e6f4877d6d4b0a4cf
foxidokun <foxido@foxido.dev>
2024-11-19 01:41:48 +0300

    load variable before using it as expression

--- src/blocks/variable.cpp
+++ src/blocks/variable.cpp
@@ -1,5 +1,6 @@
 #include <blocks/variable.h>
-#include <fmt/base.h>
+#include <fmt/format.h>
+#include <llvm/IR/Type.h>
 
 llvm::Value *VariableExpression::codegen(CompilerContext& nyacc_context, llvm::LLVMContext& context, llvm::IRBuilder<>& builder) const {
   auto res = nyacc_context.get_variable(name_);
@@ -7,5 +8,8 @@ llvm::Value *VariableExpression::codegen(CompilerContext& nyacc_context, llvm::L
     fmt::println(stderr, "Unknown variable {}", name_);
     exit(1);
   }
+
+  res = builder.CreateLoad(llvm::Type::getInt64Ty(context), res, fmt::format("loaded_{}", name_));
+
   return res;
 }
