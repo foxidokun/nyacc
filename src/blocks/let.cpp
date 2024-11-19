5c19aefbf08b34eeec30b76c16e7bf56863f0677
foxidokun <foxido@foxido.dev>
2024-11-19 12:36:51 +0300

    move alloca to entry block of function

--- src/blocks/let.cpp
+++ src/blocks/let.cpp
@@ -1,8 +1,16 @@
 #include <blocks/let.h>
 
-void LetStatement::codegen(CompilerContext& context) const {
+void LetStatement::codegen(CompilerContext &context) const {
   auto value = value_->codegen(context);
-  auto variable = context.builder.CreateAlloca(llvm::Type::getInt64Ty(context.llvm_context), nullptr, name_);
+
+  // Create alloca in the entry block of function
+  auto *parentFunction = context.builder.GetInsertBlock()->getParent();
+  // create temp builder to point to start of function
+  llvm::IRBuilder<> entry_builder(&(parentFunction->getEntryBlock()),
+                               parentFunction->getEntryBlock().begin());
+
+  auto variable = entry_builder.CreateAlloca(
+      llvm::Type::getInt64Ty(context.llvm_context), nullptr, name_);
   // Assign
   context.builder.CreateStore(value, variable);
 
