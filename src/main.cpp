4121ef498872644eda4eb8124e045181107d8c87
foxidokun <foxido@foxido.dev>
2024-11-28 17:22:53 +0300

    optimizer

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@
 #include <llvm/Support/raw_ostream.h>
 #include <parser.gen.h>
 #include <llvm/IR/Module.h>
+#include <llvm/IR/PassManager.h>
+#include <llvm/Passes/PassBuilder.h>
+#include <llvm/Analysis/LoopAnalysisManager.h>
+#include <llvm/Analysis/CGSCCPassManager.h>
 
 #include <blocks/program.h>
 #include <sstream>
@@ -31,6 +35,23 @@ std::unique_ptr<Program> get_ast(const char *filename) {
   return prog;
 }
 
+void run_optimizer(llvm::Module& module) {
+  llvm::LoopAnalysisManager LAM;
+  llvm::FunctionAnalysisManager FAM;
+  llvm::CGSCCAnalysisManager CGAM;
+  llvm::ModuleAnalysisManager MAM;
+
+  llvm::PassBuilder PB;
+  PB.registerModuleAnalyses(MAM);
+  PB.registerCGSCCAnalyses(CGAM);
+  PB.registerFunctionAnalyses(FAM);
+  PB.registerLoopAnalyses(LAM);
+  PB.crossRegisterProxies(LAM, FAM, CGAM, MAM);
+
+  llvm::ModulePassManager MPM = PB.buildPerModuleDefaultPipeline(llvm::OptimizationLevel::O2);
+  MPM.run(module,MAM);
+}
+
 int main(int argc, char **argv) {
   if (argc != 3) {
     return 1;
@@ -44,7 +65,10 @@ int main(int argc, char **argv) {
 
   CompilerContext nyacc_context(LLVMContext, LLVMBuilder, LLVMModule);
 
+  // compile
   prog->codegen(nyacc_context);
+  // optimize
+  run_optimizer(LLVMModule);
 
   std::error_code ec;
   llvm::raw_fd_ostream out_file(argv[2], ec);
